f57b5229f0d2142acf2cc5e4d548ceb60b52f1ed
Anthony Zyrek <anthonyzyrek@Anthonys-MacBook-Pro.local>
2018-05-17 10:13:12 -0700

    added scoreCheck function to see if player wins or loses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,28 +14,49 @@ $(document).ready(function () {
     //Creates random target number between 19-120, Creates random crystal values between 1-12
     targetNum = (Math.floor(Math.random() * 101) + 19);
     $("#numTarget").text(targetNum);
-
+    
     for (var i = 0; i < 4; i++) {
       crystalValues.push(Math.floor(Math.random() * 12) + 1);
     }
-
+    
+    //Click functions that add values of each crystal to the total score
     $("#crystalOne").on("click", function () {
       counter += crystalValues[0];
       $("#numCurrent").text(counter);
+      scoreCheck();
     })
     $("#crystalTwo").on("click", function () {
       counter += crystalValues[1];
       $("#numCurrent").text(counter);
+      scoreCheck();
     })
     $("#crystalThree").on("click", function () {
       counter += crystalValues[2];
       $("#numCurrent").text(counter);
+      scoreCheck();
     })
     $("#crystalFour").on("click", function () {
       counter += crystalValues[3];
       $("#numCurrent").text(counter);
+      scoreCheck();
     })
+
+    function scoreCheck() {
+      if (counter === targetNum) {
+        alert("Win!");
+        wins++;
+      }
+      else if (counter > targetNum) {
+        alert("Loss....Try Again");
+        loss++;
+      }
+    }
     
+    
+    //Check to see if player wins or loses
+    
+
+
 
 
 
