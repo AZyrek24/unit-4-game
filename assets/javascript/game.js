88d9ac9cf2099fcd0b0d8404e2aa29c1a10e2b87
Anthony Zyrek <anthonyzyrek@Anthonys-MacBook-Pro.local>
2018-05-16 21:20:43 -0700

    added jQuery click events functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,53 @@
+$(document).ready(function () {
+
 //Global Variables, Arrays
 //============================================================================================
-var wins = 0;
-var losses = 0;
-var counter = 0;
-var targetNum = 0;
-var increment = 0;
-var crystalValues = [];
-
-//Functions
-//============================================================================================
-$(document).ready(function() {
-
+  var wins = 0;
+  var losses = 0;
+  var counter = 0;
+  var targetNum = 0;
+  var crystalValues = [];
+  
+  //Functions
+  //============================================================================================
   function gameStart() {
-    //Creates random target number between 19-120
+    //Creates random target number between 19-120, Creates random crystal values between 1-12
     targetNum = (Math.floor(Math.random() * 101) + 19);
+    $("#numTarget").text(targetNum);
 
     for (var i = 0; i < 4; i++) {
       crystalValues.push(Math.floor(Math.random() * 12) + 1);
-      
     }
 
+    $("#crystalOne").on("click", function () {
+      counter += crystalValues[0];
+      console.log(counter);
+    })
+    $("#crystalTwo").on("click", function () {
+      counter += crystalValues[1];
+      console.log(counter);
+    })
+    $("#crystalThree").on("click", function () {
+      counter += crystalValues[2];
+      console.log(counter);
+    })
+    $("#crystalFour").on("click", function () {
+      counter += crystalValues[3];
+      console.log(counter);
+    })
     
-    
-    
-    
-    
+
+
+
+
+
+
+
+
     //Testing/Debugging
     console.log(targetNum);
     console.log(crystalValues);
+    console.log(counter);
   }
   //Main Process
   //============================================================================================
